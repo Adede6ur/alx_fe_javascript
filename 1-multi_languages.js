58a8cfaadeac0ad739986326768b3ece9518a04b
Adede6ur <diana.adede.akinyi@gmail.com>
2025-07-11 12:34:16 +0300

    1st

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,4 +1,5 @@
 console.log("C is fun")
-console.log("Python is  cool")
+console.log("Python is cool")
 console.log("javascript is amazinng")
+ 
  
--- 2-arguments.js
+++ 2-arguments.js
@@ -9,3 +9,4 @@ if (args[2] === undefined) {
 } else {
   console.log("Arguments found");
 }
+
