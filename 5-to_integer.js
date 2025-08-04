a170a46e64f5447d177334f4ee2cde3ccb67ded4
Adede6ur <diana.adede.akinyi@gmail.com>
2025-08-04 13:31:35 +0300

    added

---
+++ 5-to_integer.js
@@ -0,0 +1,13 @@
+const args = process.argv;
+const num=Number[args];
+console.log(arguments)
+
+if (is(NaN)){
+
+   (console.log(no ,arguments))
+} else {
+
+   console.log("No argumeent
+// if9
+// if... else...
+// if... else if... else if... else
---
+++ loop-language.js
@@ -0,0 +1,4 @@
+console.log("c is fun");
+console.log("python is cool");
+console.log("javascript is amazing");
+
